ad2610fc08190c8752601a0ad712d159b019dfab
dandistine <34634876+dandistine@users.noreply.github.com>
2026-09-20 12:45:12 -0500

    Allow the x11 event queue to block if no events are present, saving cpu time.

--- olcPixelGameEngine3.h
+++ olcPixelGameEngine3.h
@@ -5965,6 +5965,7 @@ namespace olc
 #if OLC_HOST == OLC_HOST_LINUX_X11
 
 #include <GL/gl.h>
+#include <poll.h>
 namespace X11
 {
 #include <X11/X.h>
@@ -12365,6 +12366,10 @@ namespace olc::host
             return nullptr;
         };
 
+        // Create a pollfd that we will use later to wait for events to appear on the queue
+        // preventing the event loop from becoming a busy loop
+        pollfd x11_connection_fd {.fd = ConnectionNumber(olc_Display), .events = POLLIN};
+
         X11::XEvent xev;
         while(systemActive){
             while (XPending(olc_Display))
@@ -12531,6 +12536,9 @@ namespace olc::host
                     }
                 }
             }
+
+            // Wait until an event appears on the x11 event queue file descriptor
+            poll(&x11_connection_fd, 1, -1);
         }
 
         systemActive = false;
